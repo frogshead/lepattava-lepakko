5c16b66039216bf79392c5f7308f63d3761ac1bd
Mikko Viitamäki <mikko.viitamaki@logisnext.eu>
2024-12-21 18:08:02 +0200

    full game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use bracket_lib::prelude::*;
 
 
@@ -5,11 +6,52 @@ const SCREEN_WIDTH : i32 = 80;
 const SCREEN_HEIGHT : i32 = 50;
 const FRAME_DURATION : f32 = 75.0;
 
+struct Obstacle{
+    x: i32,
+    gap_y: i32,
+    size: i32
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self{
+        let mut random = RandomNumberGenerator::new();
+        Obstacle{
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20-score)
+        }
+    }
+
+    fn render(&mut self,ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size =  self.size / 2;
+        // Draw the top half of the obstable...
+        for y in 0..self.gap_y - half_size{
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+        //...and the bottom half
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+        
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match =  player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
+    }
+}
+
 struct Player{
     x: i32,
     y: i32,
     velocity: f32
 }
+
+
 
 impl Player {
     fn new(x: i32, y: i32) -> Self {
@@ -42,11 +84,13 @@ impl Player {
 struct State {
     player: Player,
     frame_time: f32,
-    mode: GameMode
+    obstacle: Obstacle,
+    mode: GameMode,
+    score: i32,
 }
 
 enum GameMode {
-    Manu,
+    Menu,
     Playing,
     End
 }
@@ -56,7 +100,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
-            mode: GameMode::Manu,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            mode: GameMode::Menu,
+            score: 0
         } 
     }
     fn play(&mut self, ctx: &mut BTerm) {
@@ -71,7 +117,14 @@ impl State {
         }
         self.player.render(ctx);
         ctx.print(0, 0, "Paina välilyöntiä lepattaaksesi!");
-        if self.player.y > SCREEN_HEIGHT{
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x +SCREEN_WIDTH, self.score);
+        }
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player){
             self.mode = GameMode::End;
         }
         
@@ -80,7 +133,9 @@ impl State {
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -103,6 +158,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Kuolit! piip-piip!");
+        ctx.print_centered(6, &format!("Sait {} pistettä!", self.score));
         ctx.print_centered(8, "(P) Haluatko pelata uudestaan? Paina P");
         ctx.print_centered(9, "(Q) Lopeta");
 
@@ -119,7 +175,7 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
-            GameMode::Manu => self.main_menu(ctx),
+            GameMode::Menu => self.main_menu(ctx),
             GameMode::Playing => self.play(ctx),
             GameMode::End => self.dead(ctx),
         }
